8fab2bed177b6f31b67cb067ae35275444d6fa89
Kaitlyn Lee <kaitlynlee19@gmail.com>
2014-04-12 15:41:43 -0700

    Done with those coordinates

--- Butter/src/butter/usc/edu/Ramp.java
+++ Butter/src/butter/usc/edu/Ramp.java
@@ -1,15 +1,25 @@
 package butter.usc.edu;
-
+/*
+ *  Ramp class, creates a ramp given the name, freeway number, and the index of the coordinate in the respective array of path coordinates. 
+ *  @author Kaitlyn
+ */
 public class Ramp {
 	String name;
 	int freeway;
-	double latitude;
-	double longitude;
-	
+	int indexOfCoordinate;
+	//double latitude;
+	//double longitude;
+	/*
 	public Ramp(String name, int freeway, double longitude, double latitude) {
 		this.name = name;
 		this.freeway = freeway;
 		this.latitude = latitude;
 		this.longitude = longitude;
+	}
+	*/
+	public Ramp(String name, int freeway, int indexOfCoordinate) {
+		this.name = name;
+		this.freeway = freeway;
+		this.indexOfCoordinate = indexOfCoordinate;
 	}
 }
